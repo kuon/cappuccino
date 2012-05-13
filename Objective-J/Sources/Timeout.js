5c803ea3cf9f8affac8fe6b9fc2efc1cc3d822a3
Nicolas Goy <kuon@goyman.com>
2012-05-13 20:56:15 +0200

    New timeout config time functions

--- Objective-J/Sources/Timeout.js
+++ Objective-J/Sources/Timeout.js
@@ -1,5 +1,5 @@
 
-GLOBAL(objj_setTimeout)
-GLOBAL(objj_clearTimeout)
-GLOBAL(objj_setInterval)
-GLOBAL(objj_clearInterval)
+GLOBAL(objj_setTimeout) = CONFIG_SET_TIMEOUT;
+GLOBAL(objj_clearTimeout) = CONFIG_CLEAR_TIMEOUT;
+GLOBAL(objj_setInterval) = CONFIG_SET_INTERVAL;
+GLOBAL(objj_clearInterval) = CONFIG_CLEAR_INTERVAL;
