2637e70c28f323fc58549e92d3f605bf523b2c43
Nicolas Goy <kuon@goyman.com>
2012-05-14 10:39:01 +0200

    Tests

--- Objective-J/Tests/nodetest.js
+++ Objective-J/Tests/nodetest.js
@@ -2,5 +2,9 @@ require('../Build/Objective-J.NodeJS.js');
 
 CPLogRegister(CPLogDefault);
 
-objj_executeFile('main.j');
+//OBJJ_INCLUDE_PATHS = [require('path').dirname(__filename)];
+
+CPLog(OBJJ_MAIN_BUNDLE_URL);
+
+objj_executeFile('main.j', YES);
 
