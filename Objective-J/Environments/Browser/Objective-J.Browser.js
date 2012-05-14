e803da0d654b5336e767355af9e32ab61c6e502b
Nicolas Goy <kuon@goyman.com>
2012-05-14 11:04:59 +0200

    Added global object

--- Objective-J/Environments/Browser/Objective-J.Browser.js
+++ Objective-J/Environments/Browser/Objective-J.Browser.js
@@ -2,6 +2,7 @@
 #define CONFIG_XML_USE_DOM
 #define CONFIG_HTTP_USE_XMLHTTPREQUEST
 #define CONFIG_RESOLVE_MAIN_BUNDLE_FROM_PAGE_URL
+#define CONFIG_GLOBAL_OBJECT window
 
 #define CONFIG_SET_TIMEOUT window.setTimeout
 #define CONFIG_CLEAR_TIMEOUT window.clearTimeout
--- Objective-J/Environments/NodeJS/Objective-J.NodeJS.js
+++ Objective-J/Environments/NodeJS/Objective-J.NodeJS.js
@@ -1,6 +1,7 @@
 #define CONFIG_ENVIRONEMENTS ["NodeJS", "ObjJ"]
 #define CONFIG_RESOLVE_MAIN_BUNDLE_FROM_ARGV
 #define CONFIG_HTTP_USE_NODEJS
+#define CONFIG_GLOBAL_OBJECT global
 
 #define CONFIG_SET_TIMEOUT global.setTimeout
 #define CONFIG_CLEAR_TIMEOUT global.clearTimeout
