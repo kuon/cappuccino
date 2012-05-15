1df8529762cf36d97e2876353c230e5a43b9a21d
Nicolas Goy <kuon@goyman.com>
2012-05-15 15:50:50 +0200

    Added jakefile

---
+++ jakelib/utils.js
@@ -0,0 +1,21 @@
+
+jake.cpp = function(source, target, flags)
+{
+    cmd = ['gcc', '-w', '-E', '-x c', '-P'].concat(flags).concat([source, '-o', target]).join(' ');
+    jake.exec([cmd], function() {
+        console.log('[CPP] %s', source);
+        complete();
+    }, {stdout:true, stderr:true});
+}
+
+jake.subjake = function(folder, deps)
+{
+    if (deps === undefined)
+        deps = [];
+
+    task(folder, deps, function() {
+        jake.exec(['cd ' + folder + ' && jake'], function() {
+            complete();
+        }, {stdout:true, stderr:true});
+    }, {async: true});
+}
