9d75f5c71085d44d9af300e4bdd3872843d62592
Nicolas Goy <kuon@goyman.com>
2012-05-16 19:05:13 +0200

    Jakefile cleanup

--- Jakefile
+++ Jakefile
@@ -3,7 +3,18 @@ require('./jakelib/utils.js');
 desc('Create build directory');
 directory('Build');
 
+desc('Objective-J runtime');
 subjake('Objective-J', ['Build']);
+
+desc('Foundation framework');
 subjake('Foundation', ['Objective-J']);
 
+desc('Build cappuccino');
 task('default', ['Foundation']);
+
+desc('Clean the repository');
+task('clean', function ()
+{
+    console.log('# Deleting build directory');
+    jake.rmRf('Build');
+});
--- jakelib/utils.js
+++ jakelib/utils.js
@@ -3,20 +3,24 @@ global.cpp = function(source, target, flags)
 {
     flags.push('-DDEBUG');
     cmd = ['gcc', '-w', '-E', '-x c', '-P'].concat(flags).concat([source, '-o', target]).join(' ');
-    jake.exec([cmd], function() {
+
+    jake.exec([cmd], function()
+    {
         console.log('[CPP] %s', source);
         complete();
     }, {stdout:true, stderr:true});
-}
+};
 
 global.subjake = function(folder, deps)
 {
     if (deps === undefined)
         deps = [];
 
-    task(folder, deps, function() {
-        jake.exec(['cd ' + folder + ' && jake'], function() {
+    task(folder, deps, function()
+    {
+        jake.exec(['cd ' + folder + ' && jake'], function()
+        {
             complete();
         }, {stdout:true, stderr:true});
     }, {async: true});
-}
+};
