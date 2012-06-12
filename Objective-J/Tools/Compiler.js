17fc9496f34ffbdaa6aa1744245dd09e09999203
Nicolas Goy <kuon@goyman.com>
2012-06-12 17:24:48 +0200

    Removed temporarily

--- Objective-J/Tools/Compiler.js
+++ Objective-J/Tools/Compiler.js
@@ -11,17 +11,11 @@ var argv = OPTIMIST
     .usage('Compile an Objective-J file.\nUsage: $0 [options] file...')
     .alias('h', 'help')
     .default('o', 'stdout')
-    .default('unmarked', false)
-    .boolean('unmarked')
-    .default('T', true)
-    .boolean('T')
     .default('C', true)
     .boolean('C')
     .alias('C', 'compress')
     .describe('h', 'print help')
-    .describe('T', 'include type signatures in the compiled output')
     .describe('o', 'output file')
-    .describe('unmarked', "don't tag the output with @STATIC header")
     .describe('C', 'compress output with javascript minifier')
     .argv
 ;
@@ -45,8 +39,7 @@ var compiledFiles = [];
 for (var i = 0; i < argv._.length; i++)
 {
     var filePath = argv._[i],
-        fileContents,
-        executable;
+        fileContents;
 
     if (!PATH.existsSync(filePath))
     {
@@ -58,48 +51,10 @@ for (var i = 0; i < argv._.length; i++)
 
     fileContents = FILE.readFileSync(filePath).toString();
 
-    executable = objj_preprocess(fileContents, PATH.basename(filePath));
-    if (argv.C)
-    {
-        var parser = require("uglify-js").parser,
-            uglify = require("uglify-js").uglify,
-            compiledCode = executable.code(),
-            compressedCode;
-
-        compressedCode = parser.parse(compiledCode);
-        compressedCode = uglify.ast_mangle(compressedCode);
-        compressedCode = uglify.ast_squeeze(compressedCode);
-        compressedCode = uglify.gen_code(compressedCode);
-        executable.setCode(compressedCode);
-    }
-
     compiledFiles.push(
     {
-        contents: argv.unmarked ? executable.code() : executable.toMarkedString(),
+        contents: OBJJ.compile(fileContents, {minify: argv.C}),
         path: PATH.resolve(filePath)
     });
 }
 
-var output = [];
-
-if (!argv.unmarked)
-    output.push("@STATIC;1.0;");
-
-compiledFiles.forEach(function(compiledFile)
-{
-    var relativePath = PATH.basename(compiledFile.path).replace(/\.sj$/, '.j'),
-        contents = compiledFile.contents;
-
-    if (!argv.unmarked)
-    {
-        output.push("p;" + relativePath.length + ";" + relativePath);
-        output.push("t;" + contents.length + ";" + contents);
-    }
-    else
-        output.push(contents);
-});
-
-if (argv.o === 'stdout')
-    console.log(output.join(''));
-else
-    FILE.writeFileSync(argv.o, output.join(''));
